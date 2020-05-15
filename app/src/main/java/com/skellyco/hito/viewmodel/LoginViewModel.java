70e499110c269c6324ffd82964b2f83d639aab9c
schemabuoi <charyszczakg@gmail.com>
2020-05-15 17:40:45 +0200

    Update LoginViewModel.java
    Implement login method and instantiate AuthRepo

--- app/src/main/java/com/skellyco/hito/viewmodel/LoginViewModel.java
+++ app/src/main/java/com/skellyco/hito/viewmodel/LoginViewModel.java
@@ -1,4 +1,26 @@
 package com.skellyco.hito.viewmodel;
 
-public class LoginViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.skellyco.hito.core.domain.IAuthenticationRepository;
+import com.skellyco.hito.core.domain.Resource;
+import com.skellyco.hito.core.entity.User;
+import com.skellyco.hito.core.entity.dto.LoginDTO;
+import com.skellyco.hito.core.error.LoginError;
+import com.skellyco.hito.model.RepositoryProvider;
+
+public class LoginViewModel extends ViewModel {
+
+    private IAuthenticationRepository authenticationRepository;
+
+    public LoginViewModel()
+    {
+        authenticationRepository = RepositoryProvider.getAuthenticationRepository();
+    }
+
+    public LiveData<Resource<User, LoginError>> login(LoginDTO loginDTO)
+    {
+        return authenticationRepository.login(loginDTO);
+    }
 }
