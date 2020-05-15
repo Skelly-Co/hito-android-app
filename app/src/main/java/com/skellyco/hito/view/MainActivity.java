79c6045bb359828500b3d2b39ef9ee20b9903f9e
schemabuoi <charyszczakg@gmail.com>
2020-05-15 17:38:07 +0200

    Create MainActivity

---
+++ app/src/main/java/com/skellyco/hito/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.skellyco.hito.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.skellyco.hito.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
