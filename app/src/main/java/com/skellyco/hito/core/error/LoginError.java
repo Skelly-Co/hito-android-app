2544920c578a810c966c0f198ca3269c16e8b18f
schemabuoi <charyszczakg@gmail.com>
2020-05-15 21:25:31 +0200

    Update LoginError.java

--- app/src/main/java/com/skellyco/hito/core/error/LoginError.java
+++ app/src/main/java/com/skellyco/hito/core/error/LoginError.java
@@ -2,5 +2,24 @@ package com.skellyco.hito.core.error;
 
 public class LoginError {
 
+    public enum Type {
+        WRONG_PASSWORD, USER_NOT_FOUND, TOO_MANY_ATTEMPTS, NETWORK_ERROR, UNKNOWN
+    }
 
+    private Type type;
+
+    public LoginError()
+    {
+        type = Type.UNKNOWN;
+    }
+
+    public Type getType()
+    {
+        return type;
+    }
+
+    public void setType(Type type)
+    {
+        this.type = type;
+    }
 }
