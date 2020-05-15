137fff76dac9f4c2cd1f25912801769ba7c2147f
schemabuoi <charyszczakg@gmail.com>
2020-05-15 22:14:17 +0200

    Update LoginActivity.java
    Implement login method (without proper error handling yet)

--- app/src/main/java/com/skellyco/hito/view/LoginActivity.java
+++ app/src/main/java/com/skellyco/hito/view/LoginActivity.java
@@ -1,24 +1,38 @@
 package com.skellyco.hito.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.skellyco.hito.R;
+import com.skellyco.hito.core.domain.Resource;
+import com.skellyco.hito.core.entity.User;
 import com.skellyco.hito.core.entity.dto.LoginDTO;
+import com.skellyco.hito.core.error.LoginError;
+import com.skellyco.hito.view.util.LiveDataUtil;
+import com.skellyco.hito.viewmodel.LoginViewModel;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
 
     private Button btnLogin;
     private EditText etEmail;
     private EditText etPassword;
     private TextView tvCreateAccount;
     private TextView tvForgotPassword;
+
+    private LoginViewModel loginViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +41,7 @@ public class LoginActivity extends AppCompatActivity {
 
         initializeViews();
         initializeListeners();
+        initializeViewModel();
     }
 
     private void initializeViews()
@@ -65,9 +80,33 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    private void initializeViewModel()
+    {
+        loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+    }
+
     private void login(LoginDTO loginDTO)
     {
-
+        btnLogin.setEnabled(false);
+        final LiveData<Resource<User, LoginError>> loginResource = loginViewModel.login(loginDTO);
+        LiveDataUtil.observeOnce(loginResource, new Observer<Resource<User, LoginError>>() {
+            @Override
+            public void onChanged(Resource<User, LoginError> resource) {
+                if(resource.getStatus() == Resource.Status.SUCCESS)
+                {
+                    startMainActivity();
+                    finish();
+                }
+                else
+                {
+                    Toast toast = Toast.makeText(getApplicationContext(),resource.getError().getType().toString(),Toast. LENGTH_SHORT);
+                    toast. setMargin(50,50);
+                    toast. show();
+                    Log.e(TAG, "onChanged called!");
+                    btnLogin.setEnabled(true);
+                }
+            }
+        });
     }
 
     private void startCreateAccountActivity()
@@ -82,5 +121,11 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ForgotPasswordActivity.class);
         startActivity(intent);
         overridePendingTransition(R.anim.activity_slide_in_right, R.anim.activity_slide_out_left);
+    }
+
+    private void startMainActivity()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
