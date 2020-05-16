ca85844ae027b508a2733e0ea08f1e344ff00ef5
schemabuoi <charyszczakg@gmail.com>
2020-05-16 16:35:41 +0200

    Update CreateAccountError.java

--- app/src/main/java/com/skellyco/hito/core/error/CreateAccountError.java
+++ app/src/main/java/com/skellyco/hito/core/error/CreateAccountError.java
@@ -1,4 +1,30 @@
 package com.skellyco.hito.core.error;
 
 public class CreateAccountError {
+
+    public enum Type {
+        UNKNOWN
+    }
+
+    private Type type;
+
+    public CreateAccountError()
+    {
+        type = Type.UNKNOWN;
+    }
+
+    public CreateAccountError(Type type)
+    {
+        this.type = type;
+    }
+
+    public Type getType()
+    {
+        return type;
+    }
+
+    public void setType(Type type)
+    {
+        this.type = type;
+    }
 }
