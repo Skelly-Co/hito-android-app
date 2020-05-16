7977c6d585d6559607a412c164d20a33b36d0346
schemabuoi <charyszczakg@gmail.com>
2020-05-16 16:35:05 +0200

    Update CreateAccountViewModel.java
    Implement constructor & createAccount method

--- app/src/main/java/com/skellyco/hito/viewmodel/CreateAccountViewModel.java
+++ app/src/main/java/com/skellyco/hito/viewmodel/CreateAccountViewModel.java
@@ -1,4 +1,24 @@
 package com.skellyco.hito.viewmodel;
 
+import androidx.lifecycle.LiveData;
+
+import com.skellyco.hito.core.domain.IAuthenticationRepository;
+import com.skellyco.hito.core.domain.Resource;
+import com.skellyco.hito.core.entity.dto.CreateAccountDTO;
+import com.skellyco.hito.core.error.CreateAccountError;
+import com.skellyco.hito.model.RepositoryProvider;
+
 public class CreateAccountViewModel {
+
+    private IAuthenticationRepository authenticationRepository;
+
+    public CreateAccountViewModel()
+    {
+        this.authenticationRepository = RepositoryProvider.getAuthenticationRepository();
+    }
+
+    public LiveData<Resource<Void, CreateAccountError>> createAccount(CreateAccountDTO createAccountDTO)
+    {
+        return authenticationRepository.createAccount(createAccountDTO);
+    }
 }
