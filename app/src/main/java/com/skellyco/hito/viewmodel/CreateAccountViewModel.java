e54141babb22f1e652d5b64de7c99ff3e73787fb
schemabuoi <charyszczakg@gmail.com>
2020-05-17 21:47:13 +0200

    Update model dependencies

--- app/src/main/java/com/skellyco/hito/viewmodel/CreateAccountViewModel.java
+++ app/src/main/java/com/skellyco/hito/viewmodel/CreateAccountViewModel.java
@@ -3,23 +3,23 @@ package com.skellyco.hito.viewmodel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.skellyco.hito.core.domain.IAuthenticationRepository;
+import com.skellyco.hito.core.application.IAuthenticationService;
 import com.skellyco.hito.core.domain.Resource;
 import com.skellyco.hito.core.entity.dto.CreateAccountDTO;
 import com.skellyco.hito.core.error.CreateAccountError;
-import com.skellyco.hito.model.RepositoryProvider;
+import com.skellyco.hito.dependency.DependencyProvider;
 
 public class CreateAccountViewModel extends ViewModel {
 
-    private IAuthenticationRepository authenticationRepository;
+    private IAuthenticationService authenticationService;
 
     public CreateAccountViewModel()
     {
-        this.authenticationRepository = RepositoryProvider.getAuthenticationRepository();
+        this.authenticationService = DependencyProvider.getAuthenticationService();
     }
 
     public LiveData<Resource<Void, CreateAccountError>> createAccount(CreateAccountDTO createAccountDTO)
     {
-        return authenticationRepository.createAccount(createAccountDTO);
+        return authenticationService.createAccount(createAccountDTO);
     }
 }
--- app/src/main/java/com/skellyco/hito/viewmodel/LoginViewModel.java
+++ app/src/main/java/com/skellyco/hito/viewmodel/LoginViewModel.java
@@ -3,24 +3,23 @@ package com.skellyco.hito.viewmodel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.skellyco.hito.core.domain.IAuthenticationRepository;
+import com.skellyco.hito.core.application.IAuthenticationService;
 import com.skellyco.hito.core.domain.Resource;
-import com.skellyco.hito.core.entity.User;
 import com.skellyco.hito.core.entity.dto.LoginDTO;
 import com.skellyco.hito.core.error.LoginError;
-import com.skellyco.hito.model.RepositoryProvider;
+import com.skellyco.hito.dependency.DependencyProvider;
 
 public class LoginViewModel extends ViewModel {
 
-    private IAuthenticationRepository authenticationRepository;
+    private IAuthenticationService authenticationService;
 
     public LoginViewModel()
     {
-        authenticationRepository = RepositoryProvider.getAuthenticationRepository();
+        authenticationService = DependencyProvider.getAuthenticationService();
     }
 
     public LiveData<Resource<String, LoginError>> login(LoginDTO loginDTO)
     {
-        return authenticationRepository.login(loginDTO);
+        return authenticationService.login(loginDTO);
     }
 }
