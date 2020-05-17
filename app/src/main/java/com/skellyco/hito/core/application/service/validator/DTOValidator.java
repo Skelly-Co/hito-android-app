5368c667f306b87302122e64216c766b439a13a2
schemabuoi <charyszczakg@gmail.com>
2020-05-17 22:53:04 +0200

    Update DTOValidator.java
    implement validateCreateAccount error

--- app/src/main/java/com/skellyco/hito/core/application/service/validator/DTOValidator.java
+++ app/src/main/java/com/skellyco/hito/core/application/service/validator/DTOValidator.java
@@ -1,9 +1,13 @@
 package com.skellyco.hito.core.application.service.validator;
 
+import com.skellyco.hito.core.entity.dto.CreateAccountDTO;
 import com.skellyco.hito.core.entity.dto.LoginDTO;
-import com.skellyco.hito.core.error.LoginError;
+import com.skellyco.hito.core.shared.error.CreateAccountError;
+import com.skellyco.hito.core.shared.error.LoginError;
 
 import org.apache.commons.validator.routines.EmailValidator;
+
+import java.util.regex.Pattern;
 
 public class DTOValidator {
 
@@ -27,9 +31,52 @@ public class DTOValidator {
         return new ValidationResult<>(true, null);
     }
 
+    public static ValidationResult<CreateAccountError> validateCreateAccountDTO(CreateAccountDTO createAccountDTO)
+    {
+        if(createAccountDTO.getEmail() == null || createAccountDTO.getEmail().isEmpty())
+        {
+            CreateAccountError error = new CreateAccountError(CreateAccountError.Type.EMPTY_EMAIL);
+            return new ValidationResult<>(false, error);
+        }
+        if(!validateEmail(createAccountDTO.getEmail()))
+        {
+            CreateAccountError error = new CreateAccountError(CreateAccountError.Type.INVALID_EMAIL);
+            return new ValidationResult<>(false, error);
+        }
+        if(createAccountDTO.getUsername() == null || createAccountDTO.getUsername().isEmpty())
+        {
+            CreateAccountError error = new CreateAccountError(CreateAccountError.Type.EMPTY_USERNAME);
+            return new ValidationResult<>(false, error);
+        }
+        if(createAccountDTO.getPassword() == null || createAccountDTO.getPassword().isEmpty())
+        {
+            CreateAccountError error = new CreateAccountError(CreateAccountError.Type.EMPTY_PASSWORD);
+            return new ValidationResult<>(false, error);
+        }
+        if(!validatePassword(createAccountDTO.getPassword()))
+        {
+            CreateAccountError error = new CreateAccountError(CreateAccountError.Type.WEAK_PASSWORD);
+            return new ValidationResult<>(false, error);
+        }
+        return new ValidationResult<>(true, null);
+    }
+
     private static boolean validateEmail(String email)
     {
         return EmailValidator.getInstance().isValid(email);
+    }
+
+    private static boolean validatePassword(String password)
+    {
+        Pattern pattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=\\S+$).{8,}$");
+        if(pattern.matcher(password).matches())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
 }
