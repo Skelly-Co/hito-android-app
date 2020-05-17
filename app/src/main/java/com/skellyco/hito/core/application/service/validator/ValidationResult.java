60363af1fb8c7cb1eecbe66321f59267d6b2425e
schemabuoi <charyszczakg@gmail.com>
2020-05-17 20:39:21 +0200

    Create ValidationResult.java
    Create class that will be used in services as a return value of validation.

---
+++ app/src/main/java/com/skellyco/hito/core/application/service/validator/ValidationResult.java
@@ -0,0 +1,25 @@
+package com.skellyco.hito.core.application.service.validator;
+
+import com.skellyco.hito.core.error.IError;
+
+public class ValidationResult<T extends IError> {
+
+    private boolean valid;
+    private T error;
+
+    public ValidationResult(boolean valid, T error)
+    {
+        this.valid = valid;
+        this.error = error;
+    }
+
+    public boolean isValid()
+    {
+        return valid;
+    }
+
+    public T getError()
+    {
+        return error;
+    }
+}
