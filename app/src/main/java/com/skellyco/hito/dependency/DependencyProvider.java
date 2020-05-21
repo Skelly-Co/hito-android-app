78e33ba75bcd566632f7d7ee6559978074dd4749
schemabuoi <charyszczakg@gmail.com>
2020-05-21 21:43:57 +0200

    Update DependencyProvider.java
    Add UserService

--- app/src/main/java/com/skellyco/hito/dependency/DependencyProvider.java
+++ app/src/main/java/com/skellyco/hito/dependency/DependencyProvider.java
@@ -1,12 +1,16 @@
 package com.skellyco.hito.dependency;
 
 import com.skellyco.hito.core.application.IAuthenticationService;
+import com.skellyco.hito.core.application.IUserService;
 import com.skellyco.hito.core.application.service.AuthenticationService;
+import com.skellyco.hito.core.application.service.UserService;
 import com.skellyco.hito.model.firebase.AuthenticationRepository;
+import com.skellyco.hito.model.firebase.UserRepository;
 
 public class DependencyProvider {
 
     private static IAuthenticationService authenticationService;
+    private static IUserService userService;
 
     public static IAuthenticationService getAuthenticationService()
     {
@@ -15,6 +19,15 @@ public class DependencyProvider {
             authenticationService = new AuthenticationService(new AuthenticationRepository());
         }
         return authenticationService;
+    }
+
+    public static IUserService getUserService()
+    {
+        if(userService == null)
+        {
+            userService = new UserService(new UserRepository());
+        }
+        return userService;
     }
 
 }
