9ff5ad6af34a9c45bda46d33c1349e445357c914
schemabuoi <charyszczakg@gmail.com>
2020-05-21 21:44:32 +0200

    Update MainActivity.java
    Update navbar behaviour & prepare for implementing recycler view adapters

--- app/src/main/java/com/skellyco/hito/view/main/MainActivity.java
+++ app/src/main/java/com/skellyco/hito/view/main/MainActivity.java
@@ -1,25 +1,39 @@
 package com.skellyco.hito.view.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.util.Log;
 import android.view.View;
-import android.view.animation.Animation;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.skellyco.hito.R;
+import com.skellyco.hito.core.entity.User;
+import com.skellyco.hito.core.shared.Resource;
+import com.skellyco.hito.core.shared.error.FetchDataError;
 import com.skellyco.hito.view.entry.LoginActivity;
 import com.skellyco.hito.view.util.LoginDataManager;
 import com.skellyco.hito.view.util.animation.ResizeWidthAnimation;
+import com.skellyco.hito.viewmodel.main.MainViewModel;
+
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private enum NavbarItem {
+        GROUPS, LOCAL_USERS, HISTORY
+    }
+
+    public static final String TAG = "MainActivity";
+    public static final String EXTRA_UID = "EXTRA_UID";
     private static final int SETTINGS_POPUP_ANIM_DURATION = 350;
     private static final int SETTINGS_POPUP_HIDDEN_WIDTH = 1;
 
@@ -31,7 +45,12 @@ public class MainActivity extends AppCompatActivity {
     private RadioButton rbtLocalUsers;
     private RadioButton rbtHistory;
 
+    private MainViewModel mainViewModel;
     private LoginDataManager loginDataManager;
+
+    private String loggedInUid;
+    private Observer<Resource<List<User>, FetchDataError>> localUsersObserver;
+    private NavbarItem selectedNavbarItem;
     private int settingsPopupWidth;
     private boolean settingsPopupShown = false;
 
@@ -39,11 +58,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loggedInUid = getIntent().getStringExtra(EXTRA_UID);
 
         initializeViews();
-        initializeListeners();
-        initializeLoginDataManager();
         initializeSettingsPopup();
+        initializeListeners();
+        initializeViewModel();
+        initializeLoginDataManager();
+        initializeChatListObservers();
+//        rbtLocalUsers.performClick();
     }
 
     private void initializeViews()
@@ -55,6 +78,13 @@ public class MainActivity extends AppCompatActivity {
         rbtGroups = findViewById(R.id.rbtGroups);
         rbtLocalUsers = findViewById(R.id.rbtLocalUsers);
         rbtHistory = findViewById(R.id.rbtHistory);
+    }
+
+    private void initializeSettingsPopup()
+    {
+        settingsPopupWidth = linSettingsPopup.getLayoutParams().width;
+        linSettingsPopup.getLayoutParams().width = SETTINGS_POPUP_HIDDEN_WIDTH;
+        linSettingsPopup.requestLayout();
     }
 
     private void initializeListeners()
@@ -74,21 +104,38 @@ public class MainActivity extends AppCompatActivity {
         rbtGroups.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadGroups();
+                if(selectedNavbarItem != NavbarItem.GROUPS)
+                {
+                    loadGroups();
+                    selectedNavbarItem = NavbarItem.GROUPS;
+                }
             }
         });
         rbtLocalUsers.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadLocalUsers();
+                if(selectedNavbarItem != NavbarItem.LOCAL_USERS)
+                {
+                    loadLocalUsers();
+                    selectedNavbarItem = NavbarItem.LOCAL_USERS;
+                }
             }
         });
         rbtHistory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadHistory();
+                if(selectedNavbarItem != NavbarItem.HISTORY)
+                {
+                    loadHistory();
+                    selectedNavbarItem = NavbarItem.HISTORY;
+                }
             }
         });
+    }
+
+    private void initializeViewModel()
+    {
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
     }
 
     private void initializeLoginDataManager()
@@ -96,26 +143,61 @@ public class MainActivity extends AppCompatActivity {
         loginDataManager = new LoginDataManager(this);
     }
 
-    private void initializeSettingsPopup()
+    private void initializeChatListObservers()
     {
-        settingsPopupWidth = linSettingsPopup.getLayoutParams().width;
-        linSettingsPopup.getLayoutParams().width = SETTINGS_POPUP_HIDDEN_WIDTH;
-        linSettingsPopup.requestLayout();
+        localUsersObserver = new Observer<Resource<List<User>, FetchDataError>>()
+        {
+            @Override
+            public void onChanged(Resource<List<User>, FetchDataError> resource)
+            {
+                if(resource.getStatus() == Resource.Status.SUCCESS)
+                {
+                    //TO DO - update recycler view adapter
+                }
+                // For right now I have not discovered any possible errors that can happen during fetching the data.
+                // If some error will occur, it will be logged in domain layer so it won't pass unnoticed.
+                // If this will happen, I will update error handling for FetchDataError both in domain and here.
+            }
+        };
     }
 
     private void loadGroups()
     {
-
+        if(selectedNavbarItem == NavbarItem.LOCAL_USERS)
+        {
+            mainViewModel.getLocalUsers(loggedInUid).removeObserver(localUsersObserver);
+        }
+        else if (selectedNavbarItem == NavbarItem.HISTORY)
+        {
+            //TO DO - stop observing history
+        }
+        //TO DO - start observing groups
     }
 
     private void loadLocalUsers()
     {
-
+        if(selectedNavbarItem == NavbarItem.GROUPS)
+        {
+            //TO DO - stop observing groups
+        }
+        else if(selectedNavbarItem == NavbarItem.HISTORY)
+        {
+            //TO DO - stop observing history
+        }
+        mainViewModel.getLocalUsers(loggedInUid).observe(this, localUsersObserver);
     }
 
     private void loadHistory()
     {
-
+        if(selectedNavbarItem == NavbarItem.GROUPS)
+        {
+            //TO DO - stop observing groups
+        }
+        else if(selectedNavbarItem == NavbarItem.LOCAL_USERS)
+        {
+            mainViewModel.getLocalUsers(loggedInUid).removeObserver(localUsersObserver);
+        }
+        //TO DO - start observing history
     }
 
     private void toggleSettingsPopup()
