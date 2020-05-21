7d60f29eb4c385b2634e26643cfedf77dc3ea2aa
schemabuoi <charyszczakg@gmail.com>
2020-05-21 21:40:03 +0200

    Update User.java
    Implement equals & hashcode methods

--- app/src/main/java/com/skellyco/hito/core/entity/User.java
+++ app/src/main/java/com/skellyco/hito/core/entity/User.java
@@ -1,5 +1,7 @@
 package com.skellyco.hito.core.entity;
 
+import java.util.Objects;
+
 public class User {
 
     private String uid;
@@ -46,5 +48,28 @@ public class User {
     public void setUsername(String username)
     {
         this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(uid, user.uid) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(uid, email, username);
     }
 }
