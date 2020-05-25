6d0e901993491da10c157be072563d50b98258a9
schemabuoi <charyszczakg@gmail.com>
2020-05-25 18:26:25 +0200

    Update ChatViewModel.java
    Implement methods for fetching and getting private conversation

--- app/src/main/java/com/skellyco/hito/viewmodel/main/ChatViewModel.java
+++ app/src/main/java/com/skellyco/hito/viewmodel/main/ChatViewModel.java
@@ -5,7 +5,9 @@ import android.app.Activity;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.skellyco.hito.core.application.IPrivateConversationService;
 import com.skellyco.hito.core.application.IUserService;
+import com.skellyco.hito.core.entity.PrivateConversation;
 import com.skellyco.hito.core.entity.User;
 import com.skellyco.hito.core.shared.Resource;
 import com.skellyco.hito.core.shared.error.FetchDataError;
@@ -18,10 +20,13 @@ public class ChatViewModel extends ViewModel {
     private IUserService userService;
     private LiveData<Resource<User, FetchDataError>> loggedInUser;
     private LiveData<Resource<User, FetchDataError>> interlocutorUser;
+    private IPrivateConversationService privateConversationService;
+    private LiveData<Resource<PrivateConversation, FetchDataError>> privateConversation;
 
     public ChatViewModel()
     {
         userService = DependencyProvider.getUserService();
+        privateConversationService = DependencyProvider.getPrivateConversationService();
     }
 
     public void setUsers(String loggedInUid, String interlocutorUid)
@@ -30,10 +35,29 @@ public class ChatViewModel extends ViewModel {
         this.interlocutorUid = interlocutorUid;
     }
 
-    public void fetchData(Activity activity)
+    public void fetchUsers(Activity activity)
     {
         loggedInUser = userService.getUser(activity, loggedInUid);
         interlocutorUser = userService.getUser(activity, interlocutorUid);
     }
 
+    public LiveData<Resource<User, FetchDataError>> getLoggedInUser()
+    {
+        return loggedInUser;
+    }
+
+    public LiveData<Resource<User, FetchDataError>> getInterlocutorUser()
+    {
+        return interlocutorUser;
+    }
+
+    public void fetchPrivateConversation(Activity activity)
+    {
+        privateConversation = privateConversationService.getPrivateConversation(activity, loggedInUid, interlocutorUid);
+    }
+
+    public LiveData<Resource<PrivateConversation, FetchDataError>> getPrivateConversation()
+    {
+        return privateConversation;
+    }
 }
