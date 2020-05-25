ffefc5ccf31015787913fcfe29a8100d40535618
schemabuoi <charyszczakg@gmail.com>
2020-05-25 18:26:08 +0200

    Update DependencyProvider.java
    Add PrivateConversationService

--- app/src/main/java/com/skellyco/hito/dependency/DependencyProvider.java
+++ app/src/main/java/com/skellyco/hito/dependency/DependencyProvider.java
@@ -1,16 +1,20 @@
 package com.skellyco.hito.dependency;
 
 import com.skellyco.hito.core.application.IAuthenticationService;
+import com.skellyco.hito.core.application.IPrivateConversationService;
 import com.skellyco.hito.core.application.IUserService;
 import com.skellyco.hito.core.application.service.AuthenticationService;
+import com.skellyco.hito.core.application.service.PrivateConversationService;
 import com.skellyco.hito.core.application.service.UserService;
 import com.skellyco.hito.model.firebase.AuthenticationRepository;
+import com.skellyco.hito.model.firebase.PrivateConversationRepository;
 import com.skellyco.hito.model.firebase.UserRepository;
 
 public class DependencyProvider {
 
     private static IAuthenticationService authenticationService;
     private static IUserService userService;
+    private static IPrivateConversationService privateConversationService;
 
     public static IAuthenticationService getAuthenticationService()
     {
@@ -28,6 +32,15 @@ public class DependencyProvider {
             userService = new UserService(new UserRepository());
         }
         return userService;
+    }
+
+    public static IPrivateConversationService getPrivateConversationService()
+    {
+        if(privateConversationService == null)
+        {
+            privateConversationService = new PrivateConversationService(new PrivateConversationRepository());
+        }
+        return privateConversationService;
     }
 
 }
