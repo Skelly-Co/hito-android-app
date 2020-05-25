2ef939c81d928d2513aaccae638ca60414845db2
schemabuoi <charyszczakg@gmail.com>
2020-05-25 18:26:44 +0200

    Update ChatActivity.java

--- app/src/main/java/com/skellyco/hito/view/main/ChatActivity.java
+++ app/src/main/java/com/skellyco/hito/view/main/ChatActivity.java
@@ -1,6 +1,8 @@
 package com.skellyco.hito.view.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -11,16 +13,25 @@ import android.widget.TextView;
 
 import com.r0adkll.slidr.Slidr;
 import com.skellyco.hito.R;
+import com.skellyco.hito.core.entity.Message;
+import com.skellyco.hito.core.entity.PrivateConversation;
+import com.skellyco.hito.core.entity.User;
+import com.skellyco.hito.core.shared.Resource;
+import com.skellyco.hito.core.shared.error.FetchDataError;
+import com.skellyco.hito.viewmodel.main.ChatViewModel;
 
 public class ChatActivity extends AppCompatActivity {
 
     public static String TAG = "ChatActivity";
     public static String EXTRA_LOGGED_IN_UID = "EXTRA_LOGGED_IN_UID";
     public static String EXTRA_INTERLOCUTOR_UID = "INTERLOCUTOR_UID";
+    private static String MESSAGE_PROMPT = "Message to @";
 
     private ImageButton btnBack;
     private TextView tvDisplayName;
     private EditText etMessageInput;
+
+    private ChatViewModel chatViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +39,10 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
         Slidr.attach(this);
         String loggedInUid = getIntent().getStringExtra(EXTRA_LOGGED_IN_UID);
-        String id = getIntent().getStringExtra(EXTRA_INTERLOCUTOR_UID);
-        Log.e(TAG, loggedInUid + "\n" + id);
+        String interlocutorId = getIntent().getStringExtra(EXTRA_INTERLOCUTOR_UID);
 
         initializeViews();
-        initializeViewModel();
+        initializeViewModel(loggedInUid, interlocutorId);
         initializeListeners();
     }
 
@@ -43,9 +53,10 @@ public class ChatActivity extends AppCompatActivity {
         etMessageInput = findViewById(R.id.etMessageInput);
     }
 
-    private void initializeViewModel()
+    private void initializeViewModel(String loggedInUid, String interlocutorUid)
     {
-
+        chatViewModel = new ViewModelProvider(this).get(ChatViewModel.class);
+        chatViewModel.setUsers(loggedInUid, interlocutorUid);
     }
 
     private void initializeListeners()
@@ -57,4 +68,49 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        chatViewModel.fetchUsers(this);
+        chatViewModel.getInterlocutorUser().observe(this, new Observer<Resource<User, FetchDataError>>() {
+            @Override
+            public void onChanged(Resource<User, FetchDataError> resource) {
+                if(resource.getStatus() == Resource.Status.SUCCESS)
+                {
+                    tvDisplayName.setText(resource.getData().getUsername());
+                    etMessageInput.setHint(MESSAGE_PROMPT + resource.getData().getUsername());
+                    initializeMessages();
+                }
+                // For right now I have not discovered any possible errors that can happen during fetching the data.
+                // If some error will occur, it will be logged in domain layer so it won't pass unnoticed.
+                // If this will happen, I will update error handling for FetchDataError both in domain and here.
+            }
+        });
+    }
+
+    private void initializeMessages()
+    {
+        chatViewModel.fetchPrivateConversation(this);
+        chatViewModel.getPrivateConversation().observe(this, new Observer<Resource<PrivateConversation, FetchDataError>>() {
+            @Override
+            public void onChanged(Resource<PrivateConversation, FetchDataError> resource) {
+                if(resource.getStatus() == Resource.Status.SUCCESS)
+                {
+                    //We are checking here if conversation between users exists
+                    if(resource.getData() != null)
+                    {
+                        //TO DO - initialize adapter and set recyclerview
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.activity_slide_in_left, R.anim.activity_slide_out_right);
+    }
+
 }
