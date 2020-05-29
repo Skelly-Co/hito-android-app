6b564fbb90921210ed9f9a6794fb4a8ca15902dc
schemabuoi <charyszczakg@gmail.com>
2020-05-29 03:48:35 +0200

    Update documentation

--- app/src/main/java/com/skellyco/hito/ui/viewmodel/main/ChatViewModel.java
+++ app/src/main/java/com/skellyco/hito/ui/viewmodel/main/ChatViewModel.java
@@ -16,6 +16,9 @@ import com.skellyco.hito.core.shared.error.FetchDataError;
 import com.skellyco.hito.core.shared.error.InsertDataError;
 import com.skellyco.hito.dependency.DependencyProvider;
 
+/**
+ * ViewModel for ChatActivity
+ */
 public class ChatViewModel extends ViewModel {
 
     private String loggedInUid;
@@ -31,37 +34,78 @@ public class ChatViewModel extends ViewModel {
         privateConversationService = DependencyProvider.getPrivateConversationService();
     }
 
+    /**
+     * Sets the informations about users which is required for data fetching.
+     *
+     * @param loggedInUid logged in user id.
+     * @param interlocutorUid interlocutor id.
+     */
     public void setUsers(String loggedInUid, String interlocutorUid)
     {
         this.loggedInUid = loggedInUid;
         this.interlocutorUid = interlocutorUid;
     }
 
+    /**
+     * Returns logged in user id.
+     * @return logged in user id.
+     */
     public String getLoggedInUid()
     {
         return loggedInUid;
     }
 
+    /**
+     * Uses the UserService to fetch the interlocutor.
+     *
+     * @param activity activity that invokes the method.
+     */
     public void fetchInterlocutor(Activity activity)
     {
         interlocutorUser = userService.getUser(activity, interlocutorUid);
     }
 
+    /**
+     * Returns LiveData with Resource that contains information about about the status of the call,
+     * interlocutor and error (if it occurred)
+     *
+     * @return interlocutor LiveData
+     */
     public LiveData<Resource<User, FetchDataError>> getInterlocutor()
     {
         return interlocutorUser;
     }
 
+    /**
+     * Uses the PrivateConversationService to fetch the PrivateConversation.
+     *
+     * @param activity activity that invokes the method.
+     */
     public void fetchPrivateConversation(Activity activity)
     {
         privateConversation = privateConversationService.getPrivateConversation(activity, loggedInUid, interlocutorUid);
     }
 
+    /**
+     * Returns LiveData with Resource that contains information about the status of the call,
+     * PrivateConversation and error (if it occurred)
+     *
+     * @return private conversation LiveData.
+     */
     public LiveData<Resource<PrivateConversation, FetchDataError>> getPrivateConversation()
     {
         return privateConversation;
     }
 
+    /**
+     * Uses PrivateConversationService to send the message.
+     * If PrivateConversation between logged in user and interlocutor does not exist,
+     * uses PrivateConversationService to create a conversation (with message attached to the call)
+     *
+     * @param activity activity that invokes the method.
+     * @param messageDTO message form.
+     * @return LiveData with Resource that contains informations about operation status and error (if occurred).
+     */
     public LiveData<Resource<Void, InsertDataError>> sendMessage(Activity activity, MessageDTO messageDTO)
     {
         if(privateConversation.getValue().getData() == null)
--- app/src/main/java/com/skellyco/hito/ui/viewmodel/main/MainViewModel.java
+++ app/src/main/java/com/skellyco/hito/ui/viewmodel/main/MainViewModel.java
@@ -13,6 +13,9 @@ import com.skellyco.hito.dependency.DependencyProvider;
 
 import java.util.List;
 
+/**
+ * ViewModel for MainActivity.
+ */
 public class MainViewModel extends ViewModel {
 
     private IUserService userService;
@@ -24,22 +27,46 @@ public class MainViewModel extends ViewModel {
         this.userService = DependencyProvider.getUserService();
     }
 
+    /**
+     * Sets the information about logged in user which is required for data fetching.
+     *
+     * @param uid logged in user id.
+     */
     public void setLoggedInUid(String uid)
     {
         this.loggedInUid = uid;
     }
 
+    /**
+     * Uses UserService to fetch the local users.
+     * Since we haven't implemented functionality for groups & history this method
+     * only fetches local users. However in the future it will also fetch the groups
+     * and history.
+     *
+     * @param activity activity that invokes the method.
+     */
     public void fetchData(Activity activity)
     {
         localUsers = userService.getLocalUsers(activity, loggedInUid);
         //fetch groups & history
     }
 
+    /**
+     * Returns logged in user id.
+     *
+     * @return logged in user id.
+     */
     public String getLoggedInUid()
     {
         return loggedInUid;
     }
 
+    /**
+     * Returns LiveData with Resource that contains information about about the status of the call,
+     * list of local users and error (if it occurred)
+     * @param activity that invokes the method
+     * @return local users LiveData
+     */
     public LiveData<Resource<List<User>, FetchDataError>> getLocalUsers(Activity activity)
     {
         return localUsers;
